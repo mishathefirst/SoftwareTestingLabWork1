fade599ab83209c923773f6829e17b1ebbd69c17
mishathefirst <30623681+mishathefirst@users.noreply.github.com>
2021-02-23 18:45:13 +0300

    Initial commit

---
+++ src/main/java/ArctgCalculation.java
@@ -0,0 +1,21 @@
+public class ArctgCalculation {
+
+    private double result;
+
+
+    public double calculate(double arg) {
+        return ...
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public ArctgCalculation(){}
+
+
+    public ArctgCalculation(double argument) {
+        this.result = calculate(argument);
+    }
+
+}
