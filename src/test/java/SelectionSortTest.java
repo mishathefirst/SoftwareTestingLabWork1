9b129af8f7184558dae2e42a62035d80ce2f5a9b
mishathefirst <30623681+mishathefirst@users.noreply.github.com>
2021-02-24 16:48:31 +0300

    Selection Sort Tests Fixed

--- src/test/java/SelectionSortTest.java
+++ src/test/java/SelectionSortTest.java
@@ -6,6 +6,31 @@ public class SelectionSortTest {
 
     SelectionSortProgram selection = new SelectionSortProgram();
 
+    //sorted arrays points tests
+    @Test
+    public void pointCheck1() {
+        int arrayPoint = 5;
+        int[] toSortArray = {4, 1, 2, 5, 3, 3, 9, 7, 5, 4, 10, 4, 5};
+        assertEquals(arrayPoint, selection.sort(toSortArray)[8]);
+    }
+
+    @Test
+    public void pointCheck2() {
+        int arrayPoint = 10;
+        int[] toSortArray = {5, 1, 8, 10, 11, 7, 6, 3, 4, 7, 15, 17, 20};
+        assertEquals(arrayPoint, selection.sort(toSortArray)[8]);
+    }
+
+    @Test
+    public void pointCheck3() {
+        int arrayPoint = 5;
+        int[] toSortArray = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        assertEquals(arrayPoint, selection.sort(toSortArray)[5]);
+    }
+
+
+
+    //whole arrays tests
     @Test
     public void sortCheck1() {
         int[] array = {1, 2, 3, 4, 5};
