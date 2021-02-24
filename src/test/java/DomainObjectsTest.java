940c19d0d6f43244a486134dc8888e79493a84a2
mishathefirst <30623681+mishathefirst@users.noreply.github.com>
2021-02-24 18:22:20 +0300

    Domain Model Tests initialised

---
+++ src/test/java/DomainObjectsTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DomainObjectsTest {
+
+    @Before
+    public void initialise() {
+
+    }
+
+    @Test
+    public void check...1() {
+        assert
+    }
+
+}
